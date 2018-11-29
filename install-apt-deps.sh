0912a02f494dea49ee7775b0aeea6f8b2e2cfb0f
smlng <s@mlng.net>
2018-11-29 18:28:29 +0100

    fixup! refine installation of git deps

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -4,12 +4,25 @@ if [ `uname -m` = "amd64" ] || [ `uname -m` = "i386" ]
 then
     sudo apt-get install gcc-multilib g++-multilibi lib32stdc++6 lib32z1-dev
 fi
-sudo apt-get install dpkg-dev pkg-config libssl1.0.0 libssl-dev
-#cp /usr/lib/x86_64-linux-gnu/libcrypto* /tmp
+sudo apt-get install \
+    dpkg-dev \
+    pkg-config \
+    libssl-dev
 
-sudo apt-get install build-essential libdbus-1-dev libusb-1.0-0-dev flex bison libboost-dev libcmocka-dev libcmocka0 \
-     cmake libnl-3-200 libnl-genl-3-200 libnl-3-dev libnl-genl-3-dev libusb-1.0-0 \
-     libpcap-dev
-
-
+sudo apt-get install \
+    build-essential \
+    libdbus-1-dev \
+    libusb-1.0-0-dev \
+    flex \
+    bison \
+    libboost-dev \
+    libcmocka-dev \
+    libcmocka0 \
+    cmake \
+    libnl-3-200 \
+    libnl-genl-3-200 \
+    libnl-3-dev \
+    libnl-genl-3-dev \
+    libusb-1.0-0 \
+    libpcap-dev
 
